55ac9fdfdfec36f1627d4d111deb8fa1190bf641
azuma-takaaki <azuma.takaaki.1000@gmail.com>
2022-05-20 16:26:14 +0900

    Replace user_id with login_id

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,6 @@ module SessionsHelper
 
   def current_user
     if session[:user_id]
-     #@current_user = @current_user || User.find_by(id: session[:user_id])と同じ意味
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_05_20_065443) do
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "user_id"
+    t.string "login_id"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
